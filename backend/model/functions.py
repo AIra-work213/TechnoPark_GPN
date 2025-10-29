48e924eb41a50d3031bc42e9befa54d7591fc166
AIra <AIra.work213@gmail.com>
2025-10-30 01:33:01 +0300

    docs

--- backend/model/functions.py
+++ backend/model/functions.py
@@ -1,23 +1,29 @@
 
 from YandexGPT import YandexLLM
 from vectorstore import ExtraInfoDB, SummaryDB
+from pydantic import BaseModel
 
+class Document(BaseModel):
+    id: str
+    doc_name: str = ""
+    summary: str
+    extrainfo: str
+    text: str
+    is_root: bool
+    is_visited: bool = False
 
+def get_docs(docs: list[Document]):
+    return docs
+docs = get_docs()
 extra_info_db = ExtraInfoDB()
 summary_db = SummaryDB()
 super_model = YandexLLM()
 
 for doc in docs:
-    doc["summary"] = super_model.summarize_context(doc["text"])
-    doc["extrainfo"] = super_model.get_extrainfo(doc["text"])
+    doc.summary = super_model.summarize_context(doc.text)
+    doc.extrainfo = super_model.get_extrainfo(doc.text)
 
-docs = [{
-    "id": None,
-    "doc_name": None,
-    "summary": None,
-    "extrainfo": None,
-    "text": None,
-    "is_root": None,
-    "is_visited": False
-}]
+all_visited = []
+
+
 
